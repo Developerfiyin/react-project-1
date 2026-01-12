60f15af15ed303349fca1f36efb03e3de651e928
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-12 08:44:07 +0100

    import necessary icons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 import Logo from '../assets/assests/Logo.svg'
 import {HiOutlineBars3} from 'react-icons/hi2'
 import { BsCart2} from 'react-icons/bs'
-import { Box, Drawer, ListItem, ListItemButton, } from '@mui/material'
+import
+ { Box,
+     Drawer,
+      ListItem, 
+      ListItemButton,
+       ListItemIcon,
+        ListItemText} from '@mui/material'
+
+
+        import { Home } from '@mui/icons-material'
+        import { Info } from '@mui/icons-material'
+        import { CommentRounded } from '@mui/icons-material'
+        import { PhoneRounded } from '@mui/icons-material'
+        import { ShoppingCartRounded } from '@mui/icons-material'
 const Navbar = () => {
+
   return (
     <div>
         <nav className="navbar">
