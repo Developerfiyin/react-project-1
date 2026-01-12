c05c9ec26588630d3e37f8f54430befd5d067ace
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-12 22:11:19 +0100

    Added a div with the classname  of home text section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,7 +14,13 @@ const Home = () => {
           <img src={bannerBackground} alt="THE BANNER BACKGROUND Image" />
         </div>
 
-         <div className=''>
+         <div className='home-text-section' >
+          <h1 className="primary-text"> Your Favourite Food Delivered Hot and Fresh</h1>
+          <p> Healthy switcher chefs do all the prop work, like peeding, chopping & marinating, so you can cook a fresh food.</p>
+           
+           <button className='secondary-button'>
+            Order Now <BsArrowBarRight/>
+           </button>
 
          </div>
 
