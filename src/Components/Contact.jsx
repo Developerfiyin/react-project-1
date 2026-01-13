422ea15dfd761ac82423048e0e726ee7187453c5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 19:38:15 +0100

    Added a div for form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,18 +5,15 @@ const Contact = () => {
     <div className='contact-page-wrapper'>
     
         <h1 className='primary-heading'> Have questions in mind ? </h1>
-        <h1 className='primary-heading'> Let's help you. </h1>
+        <h1 className='primary-heading'> Let Us help you. </h1>
 
         <div className='contact-form-container'>
-            <form className='contact-form'>
-                <input type="text" placeholder='Full Name' required />
-                <input type="email" placeholder='Email Address' required />
-                <textarea rows="6" placeholder='Your Message' required ></textarea>
-                <button type='submit' className='secondary-button contact-page-button'> Submit </button>
-            </form>
+                <input type="email" placeholder='Email Address' required/>
+                <button type='submit' className='secondary-button'> Submit </button>
+        
         </div>
     </div>
   )
-}
+};
 
 export default Contact
