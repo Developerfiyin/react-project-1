94c30ad4e030aa158ae90e5a62c88f856cbe2396
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 12:31:50 +0100

    Updated About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import bannerBackground from '../assets/assests/about-banner-background.png'
+import bannerBackground from '../assets/assests/about-background-image.png'
 import AboutImage from '../assets/assests/about-background.png'
 import { BsFillPlayCircleFill } from 'react-icons/bs'
 
@@ -8,10 +8,18 @@ const About = () => {
   return (
     <section className='about-section-container'>
      
-     <div className="about-image-container"> 
+     <div className="about-background-image-container"> 
        <img src={AboutImage} alt="About image" />
      </div>
 
+        <div className='about-section-image-container'>
+            <img src={bannerBackground} alt="ABOUT BANNER BACKGROUND Image" />
+        </div>
+
+<div className="about-section-text-container">
+            <h2>About </h2>
+            <p>Food is an important part of diet.</p>
+        </div>
 
     </section>
   )
