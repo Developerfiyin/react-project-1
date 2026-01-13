f8b0d1cb185ecc5a7b2552b282b3d489cad61895
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 13:09:06 +0100

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Home from './Components/Home.jsx'
 import About from './Components/About.jsx'
+import Work from './Components/Work.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
       <div className="App"> 
         <Home />
         <About/>
-       
+       <Work/>
 
       </div>
      
