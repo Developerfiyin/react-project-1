3529e3867b7e15c1e4c94d9cf158108bb2894976
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 13:15:37 +0100

    Added an array of object data

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -3,7 +3,25 @@ import pickmeals from '../assets/assests/pick-meals-image.png'
 import delivery from '../assets/assests/delivery-image.png'
 import choose  from '../assets/assests/choose-image.png'
 
-const Work = () => {
+const Work = () => { 
+    const workInfoData = [
+        {
+            image: pickmeals,
+            title: 'Pick Meals',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+        },
+        {
+            image: delivery,
+            title: 'Fast Delivery', 
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+        },
+        {
+            image: choose,
+            title: 'Choose Favorite', 
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+        }
+    ]
+    
   return (
     <section className="work-section-container">
 
