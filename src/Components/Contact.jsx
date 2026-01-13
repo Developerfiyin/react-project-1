c6d8f345b021434d513368ce011d8ede7101bb3c
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 19:01:20 +0100

    Updated contact section

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='contact-section-container'>
-
+    <div className='contact-page-wrapper'>
+    
+        <h1> Have questions in mind ? Let's help </h1>
+        <p> .</p>
 
     </div>
   )
