9b953ae6ccc20302740a3e60104cb9a33b2c2465
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 17:58:43 +0100

    updated testimonials section

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import john from '../assets/assests/john-doe-image.png'
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section className="testimonials-section-wrapper">
+      <div className="testimonials-section-top">
+        <p className='primary-subheading'>Testimonials</p>
+        <h2 className="primary-heading">What are Our Customers Saying ? </h2>
+          <p className=' primary-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et veritatis deleniti, velit qui exercitationem illo nisi aspernatur 
+            atque voluptatum, sed dolorem ullam quis dolores ipsa. Vel dicta fugiat exercitationem nesciunt.</p>
+      </div>
+
+      <div className="testimonials-section-bottom">
+        <div className="testimonial-box">
+          <img src={john} alt="John Doe" />
+          <p className='primary-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et veritatis deleniti, velit qui exercitationem illo nisi aspernatur 
+            atque voluptatum, sed dolorem ullam quis dolores ipsa. Vel dicta fugiat exercitationem nesciunt.</p>
+        </div>
+      </div>
+
+      
+    </section>
   )
 }
 
