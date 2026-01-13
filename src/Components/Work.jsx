aa2ca6b41c83cd07795f1e1d61bf08531f53b30f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 13:07:19 +0100

    Updated work.jsx

---
+++ src/Components/Work.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import pickmeals from '../assets/assests/pick-meals-image.png'
+import delivery from '../assets/assests/delivery-image.png'
+import choose  from '../assets/assests/choose-image.png'
+
+const Work = () => {
+  return (
+    <section className="work-section-container">
+
+
+    </section>
+  )
+}
+
+export default Work
