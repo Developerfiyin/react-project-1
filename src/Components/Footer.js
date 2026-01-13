b616a77b934746b4e7dfb719ddfdf4f174dfc0a1
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 20:55:20 +0100

    Imported necessary icons to footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import  logo from '../assets/assests/Logo.svg'
+import { FaFacebookF } from 'react-icons/fa'
+import { BsTwitter} from 'react-icons/bs'
+import { FaLinkedinIn } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaYoutube } from 'react-icons/fa';
+
 
 const Footer = () => {
   return (
-    <div>
+<div className='footer-wra'>
 
-        
+<div>
+
+</div>
     </div>
   )
 }
