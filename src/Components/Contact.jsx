274be7e6323f4fb2d616d4a9e12a43c9566dc39f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 19:07:10 +0100

    Added a div for contact text

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,9 +4,17 @@ const Contact = () => {
   return (
     <div className='contact-page-wrapper'>
     
-        <h1> Have questions in mind ? Let's help </h1>
-        <p> .</p>
+        <h1 className='primary-heading'> Have questions in mind ? </h1>
+        <h1 className='primary-heading'> Let's help you. </h1>
 
+        <div className='contact-form-container'>
+            <form className='contact-form'>
+                <input type="text" placeholder='Full Name' required />
+                <input type="email" placeholder='Email Address' required />
+                <textarea rows="6" placeholder='Your Message' required ></textarea>
+                <button type='submit' className='secondary-button contact-page-button'> Submit </button>
+            </form>
+        </div>
     </div>
   )
 }
