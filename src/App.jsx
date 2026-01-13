4a5860a39312721bd25893e30d360c1777fcffd6
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 11:59:50 +0100

    Added a section tag to the about file

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        
+      <div className="App"> 
         <Home />
         <About/>
        
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import bannerBackground from '../assets/assests/about-banner-background.png'
+import AboutImage from '../assets/assests/about-background.png'
+import { BsFillPlayCircleFill } from 'react-icons/bs'
+
 
 const About = () => {
   return (
-    <div className='about-section-container'>
+    <section className='about-section-container'>
      
-     
-    </div>
+     <div className="about-image-container"> 
+       <img src={AboutImage} alt="About image" />
+     </div>
+
+
+    </section>
   )
 }
 
