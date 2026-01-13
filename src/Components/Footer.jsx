9dab9d61e5872a0c8f9d8dab3df868caef9d3263
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-13 21:28:33 +0100

    Updated footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import  logo from '../assets/assests/Logo.svg'
+import { FaFacebookF } from 'react-icons/fa'
+import { BsTwitter} from 'react-icons/bs'
+import { FaLinkedinIn } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaYoutube } from 'react-icons/fa';
+
+
+const Footer = () => {
+  return (
+<div className='footer-wrapper'>
+
+<div className='footer-section-one'>
+     
+     <div className='footer-logo-container'>
+        <img src={logo} alt=" The FOOTER LOGO"/>
+     </div>
+        
+        <div className='footer-icons'>
+            <FaFacebookF/>
+            <BsTwitter/>
+            <FaLinkedinIn/>
+            <FaInstagram/>
+            <FaYoutube/>
+         </div>
+
+    </div>
+</div>
+    
+  )
+};
+
+export default Footer
